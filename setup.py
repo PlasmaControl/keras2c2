847994280414b22badb328112442155dcdb9274f
Nathaniel Chen <79379842+nathanchenseanwalter@users.noreply.github.com>
2025-06-20 10:24:23 -0700

    updates to workflow

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(
-    name='keras2c2',
-    version='0.1-alpha',
-    packages=['keras2c2'],
+    name='keras2c',
+    version='0.1',
+    packages=['keras2c'],
     package_dir={'':'src'},
     install_requires=[
         # Add dependencies here
